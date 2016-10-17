3dac87001e7f1a3faf330b2ae0f7316fe39d65ed
HyeonJu Jeong <0106grace@naver.com>
2016-10-17 20:55:55 +0900

    #2 DatePicker EventProgramming

--- app/src/main/java/kr/hs/e_mirim/grace0106/simplediary/MainActivity.java
+++ app/src/main/java/kr/hs/e_mirim/grace0106/simplediary/MainActivity.java
@@ -2,12 +2,45 @@ package kr.hs.e_mirim.grace0106.simplediary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    DatePicker datePic;
+    EditText editDiary;
+    Button butSave;
+    String fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        datePic = (DatePicker)findViewById(R.id.date_picker);
+        editDiary = (EditText)findViewById(R.id.edit_content);
+        butSave = (Button)findViewById(R.id.but_save);
+
+        // 현재 날짜 구하기
+        Calendar calendar = Calendar.getInstance();
+        int nowYear = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH)+1;
+        int date = calendar.get(Calendar.DATE);
+
+        // DatePicker에 현재 날짜 설정
+        datePic.init(nowYear, month, date, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                fileName=year+"_"+(monthOfYear+1)+"_"+dayOfMonth+".txt";
+                String content = readDairy(fileName);
+                editDiary.setText(content);
+                butSave.setEnabled(true);
+            }
+        });
+    }
+
+    String readDairy(String fileName) {
+        return null;
     }
 }
