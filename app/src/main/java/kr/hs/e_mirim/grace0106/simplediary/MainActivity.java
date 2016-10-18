e8ef069b722ddec6fa35448488fcfd84b1f10994
HyeonJu Jeong <0106grace@naver.com>
2016-10-18 19:29:44 +0900

    #3 implements readDiary method

--- app/src/main/java/kr/hs/e_mirim/grace0106/simplediary/MainActivity.java
+++ app/src/main/java/kr/hs/e_mirim/grace0106/simplediary/MainActivity.java
@@ -6,6 +6,9 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
@@ -40,7 +43,20 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    String readDairy(String fileName) {
-        return null;
+    String readDairy(String fileName){
+        String diaryContents = null;
+        try {
+            FileInputStream in = openFileInput(fileName);
+            byte[] txt = new byte[500];
+            in.read(txt);
+            in.close();
+            diaryContents=new String(txt);
+            butSave.setText("수정하");
+
+        } catch (IOException e) {
+            editDiary.setHint("읽어올 일기가 없음");
+            butSave.setText("새로 저장");
+        }
+        return diaryContents;
     }
 }
